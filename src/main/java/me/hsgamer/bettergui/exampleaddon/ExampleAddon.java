1a33f59ec58a85bb7a41945a9204943545581ec1
HSGamer <huynhqtienvtag@gmail.com>
2020-02-03 18:05:26 +0700

    add some

--- src/main/java/me/hsgamer/bettergui/exampleaddon/ExampleAddon.java
+++ src/main/java/me/hsgamer/bettergui/exampleaddon/ExampleAddon.java
@@ -5,6 +5,11 @@ import me.hsgamer.bettergui.object.addon.Addon;
 public final class ExampleAddon extends Addon {
 
   @Override
+  public boolean onLoad() {
+    return true;
+  }
+
+  @Override
   public void onEnable() {
     // Enable logic
   }
