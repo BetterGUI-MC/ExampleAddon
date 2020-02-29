02b822f55c9e593cde37379e7883df5ef402cc81
HSGamer <huynhqtienvtag@gmail.com>
2020-02-29 20:29:34 +0700

    Add description on each methods

--- src/main/java/me/hsgamer/bettergui/exampleaddon/ExampleAddon.java
+++ src/main/java/me/hsgamer/bettergui/exampleaddon/ExampleAddon.java
@@ -4,21 +4,35 @@ import me.hsgamer.bettergui.object.addon.Addon;
 
 public final class ExampleAddon extends Addon {
 
+  /**
+   * Called when loading the addon
+   *
+   * @return whether the addon is loaded properly
+   */
   @Override
   public boolean onLoad() {
     return true;
   }
 
+  /**
+   * Called when enabling the addon
+   */
   @Override
   public void onEnable() {
     // Enable logic
   }
-  
+
+  /**
+   * Called after all addons were loaded
+   */
   @Override
   public void onPostEnable() {
     // Post Enable logic
   }
 
+  /**
+   * Called when disabling the addon
+   */
   @Override
   public void onDisable() {
     // Disable logic
