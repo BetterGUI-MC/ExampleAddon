bd893494aeaba8f03d8c9be2529d7f3f960d169e
hsgamer <huynhqtienvtag@gmail.com>
2023-11-03 14:00:48 +0700

    update example

--- src/main/java/me/hsgamer/bettergui/exampleaddon/ExampleAddon.java
+++ src/main/java/me/hsgamer/bettergui/exampleaddon/ExampleAddon.java
@@ -1,6 +1,6 @@
 package me.hsgamer.bettergui.exampleaddon;
 
-import me.hsgamer.hscore.bukkit.addon.PluginAddon;
+import me.hsgamer.hscore.expansion.common.Expansion;
 
-public final class ExampleAddon extends PluginAddon {
+public final class ExampleAddon implements Expansion {
 }
